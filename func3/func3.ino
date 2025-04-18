cafb538174854442678da92c40eb7c1387f0a406
Your Name <your.email@example.com>
2025-04-18 22:01:13 +0330

    first commit

---
+++ func3/func3.ino
@@ -0,0 +1,244 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+#define ADC_MIN_VALUE 13
+#define ADC_MAX_VALUE 245
+
+const int8_t tblTemperatureSensor[] = {
+    91, 83, 77, 72, 68, 65, 62, 59, 56, 54,
+    52, 50, 48, 46, 45, 43, 42, 41, 39, 38,
+    37, 36, 35, 34, 33, 32, 31, 30, 29, 28,
+    28, 27, 26, 25, 25, 24, 23, 22, 22, 21,
+    21, 20, 19, 19, 18, 18, 17, 17, 16, 15,
+    15, 14, 14, 13, 13, 13, 12, 12, 11, 11,
+    10, 10, 9, 9, 8, 8, 8, 7, 7, 6,
+    6, 6, 5, 5, 4, 4, 4, 3, 3, 3,
+    2, 2, 2, 1, 1, 1, 0, 0, -1, -1,
+    -1, -2, -2, -2, -3, -3, -3, -4, -4, -4,
+    -4, -5, -5, -5, -6, -6, -6, -7, -7, -7,
+    -8, -8, -8, -8, -9, -9, -9, -10, -10, -10,
+    -11, -11, -11, -11, -12, -12, -12, -13, -13, -13,
+    -14, -14, -14, -14, -15, -15, -15, -16, -16, -16,
+    -16, -17, -17, -17, -18, -18, -18, -19, -19, -19,
+    -19, -20, -20, -20, -21, -21, -21, -22, -22, -22,
+    -22, -23, -23, -23, -24, -24, -24, -25, -25, -25,
+    -26, -26, -26, -27, -27, -27, -28, -28, -28, -29,
+    -29, -29, -30, -30, -30, -31, -31, -32, -32, -32,
+    -33, -33, -34, -34, -34, -35, -35, -36, -36, -36,
+    -37, -37, -38, -38, -39, -39, -40, -40, -41, -41,
+    -42, -42, -43, -43, -44, -45, -45, -46, -46, -47,
+    -48, -49, -49, -50, -51, -52, -53, -54, -55, -56,
+    -57, -59, -60
+};
+
+
+struct WiFiConfig {
+  const char *ssid;
+  const char *password;
+};
+
+WiFiConfig wifi = { "TP-Link", "09143098424" };
+
+struct MQTTConfig {
+  const char *broker;
+  const char *topic;
+  const char *username;
+  const char *password;
+  int port;
+};
+
+MQTTConfig mqtt = { "broker.emqx.io", 
+                    "emqx/temp3", 
+                    "emqx",
+                    "public",
+                    8883 };
+
+struct SensorConfig {
+  const int pin;         // Sensor Pin
+  const int numReadings; // Number of samples for moving average
+  int readings[4];       // Array to store readings 
+  int currentIndex;      // Current index in array
+  long sum;              // Sum of readings
+  int average;           // Calculated average
+  float temperature;     // Temperature value
+
+  SensorConfig() 
+    : pin(A0), numReadings(4), currentIndex(0), sum(0), average(0), temperature(0.0) {
+    for (int i = 0; i < numReadings; i++) {
+      readings[i] = 0;
+    }
+  }
+};
+                    
+bool  wifi_sleeping = false;
+volatile bool shouldWakeUp = false;
+
+SensorConfig sensor;
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+const char fingerprint[] PROGMEM = "9B:98:97:3C:2B:08:5A:83:23:0C:18:B9:34:14:61:07:F6:59:78:63";
+
+//*******************************Functions**************************************//
+void setup() 
+{
+  Serial.begin(115200);
+  wiFi_INIT();
+  mqtt_INIT();
+}
+
+void loop() 
+{  
+  Serial.flush();
+  
+  goToSleep();
+  
+  if (shouldWakeUp) {
+    shouldWakeUp = false; 
+    wakeup();         
+  }
+  delay(100);
+}
+
+//*****************************************************************************//
+void callback()
+{
+  ESP.wdtFeed();
+  shouldWakeUp = true;
+  Serial.flush();
+}
+//*****************************************************************************//
+void goToSleep()
+{
+    WiFi.disconnect();
+    WiFi.forceSleepBegin();
+    delay(200);
+    
+    uint32_t sleep_time_in_ms = 1000;
+    
+    wifi_set_opmode(NULL_MODE);
+    wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+    wifi_fpm_open();
+    wifi_fpm_set_wakeup_cb(callback);
+    wifi_sleeping = true;
+    wifi_fpm_do_sleep(sleep_time_in_ms * 1000); 
+    wifi_sleeping = false;
+    esp_delay(sleep_time_in_ms + 1, [](){ return wifi_sleeping; });
+  
+}
+//*****************************************************************************//
+void wakeup()
+{
+  WiFi.forceSleepWake();
+  Serial.println("Waking up...");
+  delay(10);
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("connecting to wifi...");
+    wiFi_INIT();
+  }
+  mqtt_INIT();
+  read_average_temp();
+  publish_temp();
+}
+//*****************************************************************************//
+void publish_temp()
+{    
+  if (client.publish(mqtt.topic,String(sensor.temperature).c_str() )) 
+  {
+    Serial.print("Published temp = ");
+    Serial.println(sensor.temperature);
+  } 
+  else 
+  {
+    Serial.println("Message publish failed!");
+  }
+}
+//*****************************************************************************//
+void wiFi_INIT() 
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi.ssid, wifi.password);
+  
+  Serial.println("\nConnecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println("\nConnected to WiFi network");
+  Serial.print("ESP8266 IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+//*****************************************************************************//
+void wifi_reconnect()
+{
+  Serial.println("wifi_reconnect...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi.ssid, wifi.password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(100);
+  }
+    Serial.println("wifi connected ...");
+}
+//*****************************************************************************//
+void mqtt_INIT() 
+{
+  Serial.println("mqtt init...");
+  espClient.setFingerprint(fingerprint);
+  client.setServer(mqtt.broker, mqtt.port);
+  
+  String client_id = "esp8266-client-";
+  client_id += String(WiFi.macAddress());
+  
+  if (client.connect(client_id.c_str(), mqtt.username, mqtt.password)) 
+  {
+    Serial.println("MQTT broker connected");
+  } 
+  else
+  {
+    Serial.print("MQTT connection failed, retrying...");
+  }
+}
+//*****************************************************************************//
+void mqtt_reconnect()
+{
+  espClient.setFingerprint(fingerprint);
+  client.setServer(mqtt.broker, mqtt.port);
+  if (client.connect("ididit", mqtt.username, mqtt.password)) 
+  {
+    Serial.println("MQTT broker connected");
+  }
+  else
+  {
+    Serial.print("MQTT connection failed, retrying...");
+  }
+}
+//*****************************************************************************//
+void read_average_temp(){ 
+
+    int newReading = analogRead(sensor.pin);
+    sensor.sum -= sensor.readings[sensor.currentIndex];
+    sensor.readings[sensor.currentIndex] = newReading;
+    sensor.sum += newReading;
+    sensor.currentIndex = (sensor.currentIndex + 1) % sensor.numReadings;
+    sensor.average = sensor.sum >> 2 ;
+    sensor.temperature =   rawadc2celsius(sensor.average>>2); // shifted because the used LUT is calibrated for 8 bit adc
+}
+//*****************************************************************************//
+double rawadc2celsius(int adcValue) {
+  
+    if (adcValue < ADC_MIN_VALUE) {
+      return 99;
+    } 
+    else if (adcValue > ADC_MAX_VALUE) {
+      return -99;
+    } 
+    else {
+        return tblTemperatureSensor[adcValue - ADC_MIN_VALUE]; 
+    }
+}
+//*****************************End of code*************************************//
---
+++ subscriber/subscriber.ino
@@ -0,0 +1,250 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Ticker.h>
+#include <ESPAsyncWebServer.h>
+#include <EEPROM.h>
+#include "certificate.h"
+
+#define EEPROM_SIZE 256  
+
+struct WiFiConfig {
+  const char *ssid;
+  const char *password;
+};
+
+WiFiConfig wifi = { "TP-Link", "09143098424" };
+
+
+struct MQTTConfig {
+  char broker[32];
+  const char *topic1;
+  const char *topic2;
+  const char *topic3;
+  const char *username;
+  const char *password;
+  int port;
+};
+
+
+MQTTConfig mqtt = { "broker.emqx.io", 
+                    "emqx/temp1", 
+                    "emqx/temp2",
+                    "emqx/temp3",
+                    "emqx",
+                    "public",
+                    8883 };
+
+struct PayloadConfig {
+  int temp1;
+  int temp2;
+  int dtemp;
+  unsigned int etemp1;
+  unsigned int etemp2;
+  };
+
+PayloadConfig payload = {0, 0, 0, 0, 0}; 
+
+BearSSL::X509List *serverTrustedCA = nullptr;
+BearSSL::WiFiClientSecure espClient;
+PubSubClient client(espClient);
+Ticker publishTempDiff;
+AsyncWebServer server(80);
+
+
+//*******************************Functions**************************************//
+void setup() 
+{
+  Serial.begin(115200);
+  
+  wiFi_INIT();
+  loadBrokerIP();  
+  serverTrustedCA = new BearSSL::X509List(ca_cert);
+  mqtt_INIT();
+  publishTempDiff.attach(1,publish_temp); 
+  startServer();
+  ESP.wdtEnable(1500); 
+}
+//*****************************************************************************//
+void loop() 
+{
+  if (!client.connected()) {
+      Serial.println("MQTT disconnected, attempting to reconnect...");
+      mqtt_INIT();
+    }
+    client.loop();
+    yield();  
+}
+//*****************************************************************************//
+void loadBrokerIP() 
+{
+  EEPROM.begin(EEPROM_SIZE);
+  char firstChar = EEPROM.read(0);
+  if (firstChar == 0xFF || firstChar == 0) 
+  { 
+    String defaultIP = "broker.emqx.io";
+    strncpy(mqtt.broker, defaultIP.c_str(), 32);  
+    mqtt.broker[31] = '\0';  
+    saveBrokerIP(defaultIP);  
+  }
+  else 
+  {
+    for (int i = 0; i < 32; i++) 
+    {
+      mqtt.broker[i] = EEPROM.read(i);
+      if (mqtt.broker[i] == '\0') break; 
+    }
+  }
+  EEPROM.end();
+  Serial.print("Loaded MQTT Broker: ");
+  Serial.println(mqtt.broker);
+}
+//*****************************************************************************//
+void saveBrokerIP(String newIP) 
+{
+  EEPROM.begin(EEPROM_SIZE);
+  for (int i = 0; i < newIP.length() && i < 31; i++)
+  {
+    EEPROM.write(i, newIP[i]);
+  }
+  EEPROM.write(newIP.length() < 31 ? newIP.length() : 31, '\0');  
+  EEPROM.commit();
+  EEPROM.end();
+  strncpy(mqtt.broker, newIP.c_str(), 32);  
+  mqtt.broker[31] = '\0';  
+  Serial.print("Saved and updated MQTT Broker: ");
+  Serial.println(mqtt.broker);
+}
+//*****************************************************************************//
+// Start the web server
+void startServer() 
+{
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) 
+  {
+    String html = "<form action='/setip' method='POST'>"
+                  "MQTT Broker: <input type='text' name='ip' value='" + String(mqtt.broker) + "'>"
+                  "<input type='submit' value='Save'></form>";
+    request->send(200, "text/html", html);
+  });
+
+  server.on("/setip", HTTP_POST, [](AsyncWebServerRequest *request) 
+  {
+    if (request->hasParam("ip", true)) 
+    {
+      String newIP = request->getParam("ip", true)->value();
+      saveBrokerIP(newIP);
+      request->send(200, "text/html", "Broker IP saved! Rebooting...");
+      delay(1000);
+      ESP.restart();
+    }
+  });
+
+  server.begin();
+}
+//*****************************************************************************//
+void wiFi_INIT() 
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi.ssid, wifi.password);
+  
+  Serial.println("\nConnecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println("\nConnected to WiFi network");
+  Serial.print("ESP8266 IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+//*****************************************************************************//
+void mqtt_INIT() 
+{
+  espClient.setTrustAnchors(serverTrustedCA);
+  setClock(); // Required for X.509 validation
+  
+  client.setServer(mqtt.broker, mqtt.port);
+  client.setCallback(callback);
+  
+  String client_id = "esp8266-client-";
+  client_id += String(WiFi.macAddress());
+ 
+  if (client.connect(client_id.c_str(), mqtt.username, mqtt.password)) 
+  {
+    Serial.println("MQTT broker connected");
+    client.publish(mqtt.topic3, "ESP8266 subscribed to temp1 and temp2");
+  
+    client.subscribe(mqtt.topic1);
+    client.subscribe(mqtt.topic2);
+  } 
+  else 
+  {
+    Serial.print("MQTT connection failed, rc=");
+    Serial.print(client.state());
+    Serial.println(" - Retrying in 2s...");
+    delay(2000);
+  }
+}
+//*****************************************************************************//
+void setClock()
+{
+  configTime( 4* 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) 
+  {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
+//*****************************************************************************//
+void callback(char *topic, byte *message, unsigned int length) 
+{
+  int num = 0;
+
+  if (strcmp(topic, mqtt.topic1) == 0) 
+  {  
+    for (int i = 0; i < length; i++) { 
+      if ((char)message[i] != '0' && (char)message[i] != '.') {  
+        num = num * 10 + ((char)message[i] - '0'); 
+      }
+    }
+    payload.temp1 = num;  
+    num = 0; 
+    payload.etemp1 = 0;
+  }
+  else if (strcmp(topic, mqtt.topic2) == 0) 
+  {  
+    for (int i = 0; i < length; i++) { 
+      if ((char)message[i] != '0' && (char)message[i] != '.') {  
+        num = num * 10 + ((char)message[i] - '0');  
+      }
+    }
+    payload.temp2 = num;  
+    num = 0;  
+    payload.etemp2 = 0;
+  }
+}
+//*****************************************************************************//
+void publish_temp()
+{
+    payload.dtemp = abs(payload.temp1 - payload.temp2);  
+
+    if (client.publish(mqtt.topic3, String(payload.dtemp).c_str())) 
+    {
+    Serial.print("Published temp difference = ");
+    Serial.println(payload.dtemp);
+    } 
+    else 
+    {
+    Serial.println("Message publish failed!");
+    }
+    ESP.wdtFeed(); 
+}
+//*****************************End of code*************************************//
